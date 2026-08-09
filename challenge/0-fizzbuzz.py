d24f98c8ad043fa449d4225bf143a7ba53920ae6
agurbanova22777 <agurbanova22777@ada.edu.az>
2026-08-09 05:02:51 +0400

    .

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
-def fizzbuzz(n):
-    for i in range(1, n + 1):
-        if i % 15 == 0:
-            print("FizzBuzz", end=" ")
-        elif i % 3 == 0:
-            print("Fizz", end=" ")
-        elif i % 5 == 0:
-            print("Buzz", end=" ")
-        else:
-            print(i, end=" ")
 
-if __name__ == "__main__":
-    import sys
-    fizzbuzz(int(sys.argv[1]))
-    print()
+import sys
+
+number = int(sys.argv[1])
+
+for i in range(1, number + 1):
+    if i % 15 == 0:
+        text = "FizzBuzz"
+    elif i % 3 == 0:
+        text = "Fizz"
+    elif i % 5 == 0:
+        text = "Buzz"
+    else:
+        text = str(i)
+
+    if i < number:
+        print(text, end=" ")
+    else:
+        print(text, end="")
---
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - deletes a node at a given index
+ * @head: pointer to the head of the list
+ * @index: index of the node to delete
+ *
+ * Return: 1 if successful, -1 if it fails
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *current;
+	unsigned int i;
+
+	if (head == NULL || *head == NULL)
+		return (-1);
+
+	current = *head;
+
+	for (i = 0; i < index && current != NULL; i++)
+		current = current->next;
+
+	if (current == NULL)
+		return (-1);
+
+	if (current->prev != NULL)
+		current->prev->next = current->next;
+	else
+		*head = current->next;
+
+	if (current->next != NULL)
+		current->next->prev = current->prev;
+
+	free(current);
+
+	return (1);
+}
